969feb62a3c80d4a84f4dde6b2ae39b26ca728b5
Enio Cocja <eniococja123@gmail.com>
2025-01-20 10:19:37 +0100

    Done tests with JUNIT

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add() {
+        assertEquals(7, calculator.add(5, 2));
+        assertNotEquals(7, calculator.add(5, 1));
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(3, calculator.subtract(5, 2));
+        assertNotEquals(3, calculator.subtract(5, 1));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(4, calculator.multiply(2, 2));
+        assertNotEquals(4, calculator.multiply(5, 1));
+    }
+
+    @Test
+    void divide() {
+            assertEquals(2, calculator.divide(10, 5));
+    }
+}
